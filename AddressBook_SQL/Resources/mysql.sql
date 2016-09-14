bb49014983bdaa61e5af8057c94d63dace8a772a
Saq1194 <ter-saq@mail.ru>
2016-09-15 02:03:54 +0400

    SQL Connection

--- AddressBook_SQL/Resources/mysql.sql
+++
@@ -1,2 +0,0 @@
-#UPDATE phonebook.users SET user_name='',user_password='' WHERE user_id=1;
-#DELETE FROM phonebook.users WHERE user_id=1;
--- AddressBook_SQL/src/am/aca/addressbook/comman/exception/InvalidArgumentException.java
+++
@@ -1,7 +0,0 @@
-package am.aca.addressbook.comman.exception;
-
-public class InvalidArgumentException extends Exception {
-    public InvalidArgumentException(String message) {
-        super(message);
-    }
-}
